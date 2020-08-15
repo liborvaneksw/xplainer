106e077e0ac886f773eea7fa62363543b7f6857a
Libor Vanek <mail@liborvanek.software>
2020-08-15 10:30:23 +0200

    setup name + descriptions update

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 with open("requirements.txt") as f:
     install_requirements = f.read().splitlines()
 
+long_description = """
+The ultimate goal of this toolkit is to promote explainable AI. 
+It should offer developers varied ways to examine predictions of their models.
+
+We are just at the beginning of a long road. For a start, we decided to aim at:
+- Image classification.
+- Tensorflow 2+ models.
+
+There are already a couple of existing libraries. We do not want to duplicate them.
+This project rather tries to make them available with as little effort as possible,
+at one place.
+
+For more information, please visit the [GitHub repository](https://github.com/liborvaneksw/xplainer/).
+"""
+
 setuptools.setup(
-    name="xplainer toolkit",
+    name="xplainer",
     version="0.0.1",
     author="Libor Vanek",
     author_email="libor@xplainer.ai",
-    description="Explainable AI made easy. Simple browser based app that enables user "
-                + "to explore their computer vision TensorFlow models predictions.",
+    description="xplainer.ai toolkit enables developers to explore predictions "
+    "of their deep learning models. Focused on image classification and TensorFlow 2+. ",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/liborvaneksw/xplainer",
@@ -21,10 +33,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     packages=setuptools.find_packages(),
     install_requires=install_requirements,
     zip_safe=False,
     include_package_data=True,
-    scripts=['bin/xplainer'],
+    scripts=["bin/xplainer"],
 )
